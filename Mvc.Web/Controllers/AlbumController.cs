e5d6f488805e12932345fa9d4448d625b3fd0d04
DaliusPamparas <dalius.pamparas@gmail.com>
2017-03-27 22:21:18 +0200

    Added Albums list + create

--- Mvc.Web/Controllers/AlbumController.cs
+++ Mvc.Web/Controllers/AlbumController.cs
@@ -29,30 +29,34 @@ namespace Mvc.Web.Controllers
             return View(allPhotosFromDB);
         }
 
-       
 
-        // GET: Album/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
 
-        // POST: Album/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "AlbumId,Name")] Album album)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Albums.Add(album);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Album album = new Album()
+            {
 
-        //    return View(album);
-        //}
+            };
+
+            return View(album);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Album model)
+        {
+            Album newAlbum = new Data.Models.Album()
+            {
+                Name = model.Name
+            };
+            albumRepo.Add(newAlbum);
+
+            
+
+            // return to home index too see the image you have uploaded
+            return RedirectToAction("index", "album");
+        }
 
         // GET: Album/Edit/5
         //public ActionResult Edit(int? id)
